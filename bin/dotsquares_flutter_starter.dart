d302af8bf3d3e3f3cae586bbe14c0ddee4d4fae6
Pradeep <bitbucket@dotsquares.com>
2025-03-05 13:07:32 +0530

    commit with api services, api repositories files and deps

--- bin/dotsquares_flutter_starter.dart
+++ bin/dotsquares_flutter_starter.dart
@@ -30,7 +30,7 @@ void main(List<String> arguments) async {
   // Define the folder structure under lib/src
   String libPath = '$projectName/lib/src';
   List<String> folders = [
-    'data/dataSources',
+    'data/dataSources/remote',
     'data/repositories',
     'domain/models',
     'domain/repositories',
@@ -50,8 +50,11 @@ void main(List<String> arguments) async {
     print('Created folder: $libPath/$folder');
   }
 
-  // Create apiEnvironment.dart file
+  // Create necessary files
   createApiEnvironmentFile('$libPath/utils/constants/apiEnvironment.dart');
+  createDataStateFile('$libPath/utils/resources/data_state.dart');
+  createApiRepositoryFile('$libPath/domain/repositories/api_repositories.dart');
+  createApiServiceFile('$libPath/data/dataSources/remote/api_service.dart');
 
   // Add dependencies to pubspec.yaml
   addDependencies(projectName);
@@ -80,26 +83,114 @@ class ApiEnvironment {
   }
 }
 
-void addDependencies(String projectPath) {
-  // Path to the pubspec.yaml file
-  var pubspecPath = '$projectPath/pubspec.yaml';
+// Function to create data_state.dart
+void createDataStateFile(String filePath) {
+  File dataStateFile = File(filePath);
 
-  // Read the existing pubspec.yaml content
+  if (!dataStateFile.existsSync()) {
+    dataStateFile.writeAsStringSync('''
+import 'package:dio/dio.dart';
+
+abstract class DataState<T> {
+  T? data;
+  DioException? exception;
+  DataState({this.data, this.exception});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  DataFailed(DioException exception) : super(exception: exception);
+}
+''');
+    print('Created file: $filePath');
+  }
+}
+
+// Function to create api_repositories.dart
+void createApiRepositoryFile(String filePath) {
+  File apiRepoFile = File(filePath);
+
+  if (!apiRepoFile.existsSync()) {
+    apiRepoFile.writeAsStringSync('''
+abstract class ApiRepository {
+  // Future<DataState<SignInResponseModel>> signIn({required SignInRequestModel signInRequestModel});
+}
+''');
+    print('Created file: $filePath');
+  }
+}
+
+// Function to create api_service.dart
+void createApiServiceFile(String filePath) {
+  File apiServiceFile = File(filePath);
+
+  if (!apiServiceFile.existsSync()) {
+    apiServiceFile.writeAsStringSync('''
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import 'package:http/http.dart' as http;
+import '../../utils/constants/apiEnvironment.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(
+    baseUrl: ApiEnvironment.baseUrl + ApiEnvironment.apiPrefix,
+    parser: Parser.JsonSerializable)
+abstract class ApiService {
+  //@POST(ApiEnvironment.signIn)
+  //Future<HttpResponse<SignInResponseModel>> signIn({
+   // @Body() required SignInRequestModel signInRequestModel,
+  //});
+}
+''');
+    print('Created file: $filePath');
+  }
+}
+
+void addDependencies(String projectPath) {
+  var pubspecPath = '$projectPath/pubspec.yaml';
   var pubspecFile = File(pubspecPath);
   var pubspecContent = pubspecFile.readAsStringSync();
 
-  // Add necessary dependencies
-  if (!pubspecContent.contains('dotsquares_flutter_starter')) {
+  // Ensure dependencies exist and add required ones dynamically
+  if (!pubspecContent.contains('dependencies:')) {
+    pubspecContent += '\ndependencies:\n';
+  }
+
+  if (!pubspecContent.contains('dotsquares_flutter_starter:')) {
     pubspecContent = pubspecContent.replaceFirst(
       'dependencies:',
       '''
 dependencies:
-  dotsquares_flutter_starter: ^1.0.0
+  dotsquares_flutter_starter: any
+  dio: any
+  retrofit: any
+  json_annotation: any
+  http: any
 ''',
     );
-    pubspecFile.writeAsStringSync(pubspecContent);
-    print('Added dotsquares_flutter_starter to pubspec.yaml');
   }
+
+  // Ensure dev_dependencies exist and add required ones dynamically
+  if (!pubspecContent.contains('dev_dependencies:')) {
+    pubspecContent += '\ndev_dependencies:\n';
+  }
+
+  if (!pubspecContent.contains('retrofit_generator:')) {
+    pubspecContent = pubspecContent.replaceFirst('dev_dependencies:', 'dev_dependencies:\n  retrofit_generator: any');
+  }
+  if (!pubspecContent.contains('build_runner:')) {
+    pubspecContent = pubspecContent.replaceFirst('dev_dependencies:', 'dev_dependencies:\n  build_runner: any');
+  }
+  if (!pubspecContent.contains('json_serializable:')) {
+    pubspecContent = pubspecContent.replaceFirst('dev_dependencies:', 'dev_dependencies:\n  json_serializable: any');
+  }
+
+  pubspecFile.writeAsStringSync(pubspecContent);
+  print('Updated pubspec.yaml with required dependencies and dev_dependencies');
 }
 
 Future<void> runFlutterPubGet(String projectPath) async {
