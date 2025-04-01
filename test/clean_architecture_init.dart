1c1a3175f475ce79080f066beee40d72b4201b48
Himanshu <himanshu.jangir@dotsquares.com>
2025-04-01 16:05:26 +0530

    changes in the file structure and logics

---
+++ test/clean_architecture_init.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('Example test', () {
+    expect(2 + 2, equals(4)); // Replace with actual logic
+  });
+}
--- test/dotsquares_flutter_starter_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:clean_architecture_init/clean_architecture_init.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
